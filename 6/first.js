a6f68840a15b0a874967692ee914823fe7ff5768
Manuel Madeira <manuel.madeira@ynap.com>
2018-12-26 15:10:33 +0000

    Add first part of day 6.

---
+++ 6/first.js
@@ -0,0 +1,85 @@
+/*
+The device on your wrist beeps several times, and once again you feel like you're falling.
+
+"Situation critical," the device announces. "Destination indeterminate. Chronal interference detected. Please specify new target coordinates."
+
+The device then produces a list of coordinates (your puzzle input). Are they places it thinks are safe or dangerous? It recommends you check manual page 729. The Elves did not give you a manual.
+
+If they're dangerous, maybe you can minimize the danger by finding the coordinate that gives the largest distance from the other points.
+
+Using only the Manhattan distance, determine the area around each coordinate by counting the number of integer X,Y locations that are closest to that coordinate (and aren't tied in distance to any other coordinate).
+
+Your goal is to find the size of the largest area that isn't infinite. For example, consider the following list of coordinates:
+
+1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9
+If we name these coordinates A through F, we can draw them on a grid, putting 0,0 at the top left:
+
+..........
+.A........
+..........
+........C.
+...D......
+.....E....
+.B........
+..........
+..........
+........F.
+This view is partial - the actual grid extends infinitely in all directions. Using the Manhattan distance, each location's closest coordinate can be determined, shown here in lowercase:
+
+aaaaa.cccc
+aAaaa.cccc
+aaaddecccc
+aadddeccCc
+..dDdeeccc
+bb.deEeecc
+bBb.eeee..
+bbb.eeefff
+bbb.eeffff
+bbb.ffffFf
+Locations shown as . are equally far from two or more coordinates, and so they don't count as being closest to any.
+
+In this example, the areas of coordinates A, B, C, and F are infinite - while not shown here, their areas extend forever outside the visible grid. However, the areas of coordinates D and E are finite: D is closest to 9 locations, and E is closest to 17 (both including the coordinate's location itself). Therefore, in this example, the size of the largest area is 17.
+
+What is the size of the largest area that isn't infinite?
+*/
+
+const utils = require('./utils')
+const { input, createMatrix, findNearestPoint } = utils
+const { matrix, points } = createMatrix(input)
+
+// calculate nearest points
+for (let i = 0; i < matrix.length; i++) {
+  const columns = matrix[i].length + 1
+  for (let j = 0; j < columns; j++) {
+    if (!matrix[i][j]) {
+      matrix[i][j] = findNearestPoint(points, i, j)
+    }
+  }
+}
+
+const infinite = {}
+const areasSum = {}
+
+for (let i = 0, len = matrix.length; i < len; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    let point = matrix[i][j].toLowerCase()
+
+    // if it's in one of the border, will be an infinite area
+    if (i === 0 || i === matrix.length - 1 || j === 0 || j === matrix[i][j].length - 1) {
+      infinite[point] = true
+    } else {
+      areasSum[point] = (areasSum[point] ? areasSum[point] + 1 : 1)
+    }
+  }
+}
+
+const largestArea = Object.keys(areasSum).reduce((acc, key) => {
+  return (!infinite[key] && areasSum[key] > acc) ? areasSum[key] : acc
+}, 0)
+
+console.log(`Largest Area: ${largestArea}`)
---
+++ 6/utils.js
@@ -0,0 +1,132 @@
+const testInput = [
+  [1, 1],
+  [1, 6],
+  [8, 3],
+  [3, 4],
+  [5, 5],
+  [8, 9]
+]
+
+const input = [
+  [242, 164],
+  [275, 358],
+  [244, 318],
+  [301, 335],
+  [310, 234],
+  [159, 270],
+  [82, 142],
+  [229, 286],
+  [339, 256],
+  [305, 358],
+  [224, 339],
+  [266, 253],
+  [67, 53],
+  [100, 143],
+  [64, 294],
+  [336, 303],
+  [261, 267],
+  [202, 86],
+  [273, 43],
+  [115, 256],
+  [78, 356],
+  [91, 234],
+  [114, 146],
+  [114, 260],
+  [353, 346],
+  [336, 283],
+  [312, 341],
+  [234, 119],
+  [281, 232],
+  [65, 203],
+  [95, 85],
+  [328, 72],
+  [285, 279],
+  [61, 123],
+  [225, 179],
+  [97, 140],
+  [329, 305],
+  [236, 337],
+  [277, 110],
+  [321, 335],
+  [261, 258],
+  [304, 190],
+  [41, 95],
+  [348, 53],
+  [226, 298],
+  [263, 187],
+  [106, 338],
+  [166, 169],
+  [310, 295],
+  [236, 191]
+]
+
+const printMatrix = (matrix) => {
+  for (let i = 0, len = matrix.length; i < len; i++) {
+    let row = ''
+    for (let j = 0; j <= matrix[i].length; j++) {
+      row += matrix[i][j] || '. '
+    }
+    console.log(row)
+  }
+}
+
+const createMatrix = (input) => {
+  const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const points = {}
+  let maxX = 0
+  let maxY = 0
+  let matrix = null
+  let counter = 0
+
+  input.forEach(([y, x]) => {
+    maxX = x > maxX ? x : maxX
+    maxY = y > maxY ? y : maxY
+  })
+
+  matrix = new Array(maxX)
+  for (let i = 0, len = maxX + 1; i < len; i++) {
+    matrix[i] = new Array(maxY + 1)
+  }
+
+  input.forEach(([y, x]) => {
+    const key = `${ALPHABET[counter % ALPHABET.length]}${counter}`
+    matrix[x][y] = key
+    points[key] = [y, x]
+    counter +=1
+  })
+
+  return { matrix, points }
+}
+
+const findNearestPoint = (points, x1, y1) => {
+  let best = null
+  let bestKey = null
+  let counter = 0
+
+  Object.keys(points).forEach((key) => {
+    const [y2, x2] = points[key]
+    const value = Math.abs(x1 - x2) + Math.abs(y1 - y2)
+
+    if (!best || value < best) {
+      best = value
+      bestKey = key.toLowerCase()
+      counter = 1
+    } else if (value === best) {
+      counter += 1
+    }
+  })
+
+  if (counter === 1) {
+    return bestKey
+  }
+
+  return '. '
+}
+
+module.exports = {
+  testInput,
+  input,
+  printMatrix,
+  createMatrix,
+  findNearestPoint
+}
